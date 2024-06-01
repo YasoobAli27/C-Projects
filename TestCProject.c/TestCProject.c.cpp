2d508ce6534d7cab7e0adefad38f1bd27930471b
YasoobAli27 <71242622+YasoobAli27@users.noreply.github.com>
2024-06-01 10:30:13 -0400

    Add files via upload

---
+++ TestCProject.c/TestCProject.c.cpp
@@ -0,0 +1,21 @@
+// TestCProject.c.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int age = 30;
+    int* pAge = &age; //memory address of age variable
+    
+    double gpa = 3.4;
+    double* pGpa = &gpa;
+    
+    char grade = 'A';
+    char* pGrade = &grade;
+
+    
+    
+}
+
+
